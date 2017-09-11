2f304bdec1bf9dde011e2eef351934158c6ebb1d
dguis <guistiboys4@gmail.com>
2017-09-11 15:41:21 -0700

    Add icon

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ var show = function(item){
   var text = document.getElementById('textbox');
   var char = document.getElementById('char');
   var sum = document.getElementById('sum');
+  var title = document.getElementById('title');
 if (item == 1) {
   text.innerHTML = "The characters include Cole, Dalton, Jenna, and Ansel.<br><br><a href='Report.docx' class='copyright' download=''>Click here to download Microsoft Word Document</a>";
   char.className = "selected";
@@ -15,4 +16,7 @@ char.className = "buttons";
 text.innerHTML = "Nothing yet!";
   }
 }
+}
+var Welcome = function(){
+alert("Welcome! This is my site for my book report project on Brandon Mull's book, Sky Raiders. I hope you enjoy it!\n-Dallin Guisti");
 }
