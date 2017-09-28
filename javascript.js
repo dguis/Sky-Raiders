cb367b8519e105fb767626c75b5de9f1ecae92fa
dguis <guistiboys4@gmail.com>
2017-09-28 16:01:01 -0700

    Upload changes to site

--- javascript.js
+++ javascript.js
@@ -26,7 +26,7 @@ var show = function (item) {
     }
     else{
       if (item == 3){
-        text.innerHTML = "Conflict";
+        text.innerHTML = "<p>The characters' goal is to overthrow the king of <span onClick='scrollWin();'>the Outskirts</span> and save many kidnapped children. The problem is that they were all branded as slaves and have many armed soldiers trying to capture them.</p>";
         set.className = "buttons";
         char.className = "buttons";
         con.className = "selected";
@@ -35,7 +35,7 @@ var show = function (item) {
       }
       else {
         if (item == 4){
-          text.innerHTML = "Main Idea";
+          text.innerHTML = "<p>The story was about a few slave children that live in a magical land called the Outskirts who were forced to do dangerous jobs until they escaped to go overthrow the king. A few slaves, such as Dalton and Jenna, were sent to the king and from there sent to anonymous places around the land. Another group of slaves, including Cole, Mira, and a few others, were sold to a company called the Sky Raiders, from where this group escaped and went on the run from the king, potentially trying to destroy him.</p>";
           set.className = "buttons";
           char.className = "buttons";
           con.className = "buttons";
@@ -43,7 +43,8 @@ var show = function (item) {
           sum.className = "buttons";
         }
         else{
-          text.innerHTML = "Summary";
+          var conb = document.getElementById('conb').innerHTML;
+          text.innerHTML = conb;
           set.className = "buttons";
           char.className = "buttons";
           con.className = "buttons";
@@ -76,8 +77,6 @@ char.className = "buttons";
 var Welcome = function(){
 alert("Welcome! This is my site for my book report project on Brandon Mull's book, Sky Raiders. I hope you enjoy it!\n-Dallin Guisti");
 }*/
-set
-char
-con
-main
-sum
+function scrollWin() {
+    window.scrollTo(0, document.getElementById('words').getBoundingClientRect().top-350);
+}
